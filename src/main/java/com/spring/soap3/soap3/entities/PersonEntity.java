5bbbe9b50ad67008324d1266e0541c1139f42008
Ndedi Georges Patrick Nkollo (DHL DE), external <n.patrick_nkollo@deutschepost.de>
2023-05-07 15:21:24 +0200

    comment changed!

--- src/main/java/com/spring/soap3/soap3/entities/PersonEntity.java
+++ src/main/java/com/spring/soap3/soap3/entities/PersonEntity.java
@@ -12,9 +12,9 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Getter
 @Setter
-@Table(name="Person") //je fais correspondre mon entite de mapping a la table person dans la base de donnees
-// sans cela la list que je demnde de la BD sera vide vu qu'aucun tableau du nom de PersonEntity n'existe dans DB (voir  H2-console ou db.migration)
-//Attention cette entite ci-presente est creer pour permettre a la classe Person (generée automatiquement par 'mvn compile') de pouvoir correspondre avec
+@Table(name="Person") //je fais correspondre cette entite ci-dessous a la table person dans la base de donnees
+// sans cela la liste que je demande de la BD sera vide vu qu'aucun tableau du nom de PersonEntity n'existe dans DB (voir  H2-console ou db.migration)
+//Attention cette entite ci-presente est crée pour permettre a la classe Person (generée automatiquement par 'mvn compile' à partir de Person.xsd) de pouvoir correspondre avec
 //la BD. Ainsi on a un code hybride SOAP(Person)/REST(PersonEntity) et les deux classes se mappent dans le service PersonRepository.
 public class PersonEntity {
 
